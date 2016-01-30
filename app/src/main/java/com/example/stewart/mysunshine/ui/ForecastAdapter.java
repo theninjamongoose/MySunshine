2ca955dc0e8b6418042b690a1e0b6f003dad0fbe
Shane Stewart <Shane.Stewart@nike.com>
2016-01-29 18:56:49 -0800

    progress

--- app/src/main/java/com/example/stewart/mysunshine/ui/ForecastAdapter.java
+++ app/src/main/java/com/example/stewart/mysunshine/ui/ForecastAdapter.java
@@ -1,8 +1,14 @@
 package com.example.stewart.mysunshine.ui;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.example.stewart.mysunshine.R;
 import com.example.stewart.mysunshine.model.Forecast;
 
 /**
@@ -10,6 +16,8 @@ import com.example.stewart.mysunshine.model.Forecast;
  */
 public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ViewHolder>{
     private Forecast mForecast;
+
+
 
     public ForecastAdapter(Forecast forecast) {
         mForecast = forecast;
@@ -28,26 +36,43 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ViewHo
     // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView mTextView;
-        public ViewHolder(TextView v) {
+        public ImageView mWeatherIcon;
+        public TextView mDailyTempRange;
+        public TextView mDayOfWeek;
+        public ViewHolder(RelativeLayout v) {
             super(v);
-            mTextView = v;
+            mWeatherIcon = (ImageView) v.findViewById(R.id.weather_icon_image_view);
+            mDailyTempRange = (TextView) v.findViewById(R.id.day_temp_range_text_view);
+            mDayOfWeek = (TextView) v.findViewById(R.id.day_of_week_text_view);
         }
     }
 
 
     @Override
     public ForecastAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.daily_forecast_row, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        mWeatherIcon = (ImageView) v.findViewById(R.id.weather_icon_image_view);
+        m
+
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(ForecastAdapter.ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        //set shit here
 
+//        holder.mTextView.setText(mDataset[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mForecast.getList().size();
     }
 }
